21eea43ff614b9614be20e280fa50e926c5b26f1
rafshanDev90 <inspiremedaily47@gmail.com>
2026-08-26 20:11:07 +0600

    feat: add session management with refresh token functionality and cookie handling

--- auth-API/src/controller/auth.controller.js
+++ auth-API/src/controller/auth.controller.js
@@ -1,4 +1,6 @@
 import userModel from '../models/user.model.js';
+import sessionModel from '../models/session.model.js';
+
 import crypto from 'crypto';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
@@ -21,22 +23,95 @@ export async function register(req, res){
     }
     const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
 
-    const newUser = await userModel.create({
+    const user = await userModel.create({
         username: username,
         email: email,
         password: hashedPassword
     });
-    res.status(201).json({message: 'User created successfully', user: newUser});
+    //res.status(201).json({message: 'User created successfully', user: user});
 
-    const token = jwt.sign({
-        id: newUser._id,
-        username: newUser.username,
-        email: newUser.email
+    const accessToken = jwt.sign({
+        id: user._id,
+        username: user.username,
+        email: user.email
     }, process.env.JWT_SECRET, {expiresIn: '1h'});
+    
+    const refreshToken = jwt.sign({
+        id: user._id,
+        username: user.username,
+        email: user.email
+    }, process.env.JWT_SECRET, {expiresIn: '7d'});
+    
+    const refreshTokenhash = crypto.randomBytes(64).toString('hex');
+    const session = await sessionModel.create({
+        user: user._id,
+        refreshToken: refreshTokenhash,
+        ip: req.ip,
+        userAgent: req.headers['user-agent']
+
+    })
+
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    });
 
     res.status(201).json({
         message: 'User created successfully',
-        user: newUser,
-        token: token
+        user: user,
+        accessToken: accessToken
+    })
+}
+
+export async function getMe(req, res){
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({message: 'No token provided'});
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    //console.log(decoded);
+
+    const user = await userModel.findById(decoded.id);
+
+    res.status(200).json({
+        message: "User featch successfully",
+        user:{
+            username: user.username,
+            email: user.email
+        }
+    })
+
+}
+
+export async function refreshToken(req, res){
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+        return res.status(401).json({message: 'No refresh token provided'});
+    }
+
+    const decoded = jwt.verify(refreshToken, process.env.JWT_SECRET);
+    const accessToken = jwt.sign({
+        id: decoded.id,
+    }, process.env.JWT_SECRET, {expiresIn: '1h'});
+    
+    const newRefreshToken = jwt.sign({
+        id: decoded.id,
+    }, process.env.JWT_SECRET, {expiresIn: '7d'});
+
+    res.cookie('refreshToken', newRefreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    });
+
+    res.status(200).json({
+        message: 'Token refreshed successfully',
+        accessToken: accessToken
     })
 }
--- auth-API/src/routes/auth.routes.js
+++ auth-API/src/routes/auth.routes.js
@@ -3,5 +3,7 @@ import * as authController from "../controller/auth.controller.js";
 
 const authRouter = router();
 authRouter.post("/register", authController.register);
+authRouter.get("/me", authController.getMe);
+authRouter.get("/refresh", authController.refreshToken);
 
 export default authRouter;
